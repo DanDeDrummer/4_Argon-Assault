908636b9359c64dd0e1182d18583eb9086afcf5e
Daniël le Roux <danielleroux25@gmail.com>
2020-05-02 15:39:18 +0200

    4_AA_Setup Splash with music and space Toggle

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            LoadFirstScene();
+        }
+    }
+
+
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
