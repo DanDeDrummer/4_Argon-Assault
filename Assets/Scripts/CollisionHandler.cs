4d3d869a370c422cd75f3cd4930d2b26521032f7
Daniël le Roux <danielleroux25@gmail.com>
2020-05-05 13:51:51 +0200

    4_AA_Added explosion effecct to ship. Reload level on death

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,17 +2,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
     }
 
     private void StartDeathSequence()
-    {
+    {       
         print("Player Dying");
         SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadScene",2f);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+        print("Reloaded Scene");
     }
 }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -24,4 +24,6 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
+
+
 }
