595a2601116a71c32fcc1bef9d8ec61c613b98aa
Daniël le Roux <danielleroux25@gmail.com>
2020-05-06 11:33:56 +0200

    4_AA_Making Enemies add own box colliders

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private void Start()
+    {
+        AddNonTriggerBoxCollider(); 
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider nonTriggerCollider = gameObject.AddComponent<BoxCollider>();
+        nonTriggerCollider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         print("It hit" + gameObject.name);
